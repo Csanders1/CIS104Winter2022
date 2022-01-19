4373bc6433d35b1f294615d67a78bdd7ac9a6be8
Csanders1 <97995168+Csanders1@users.noreply.github.com>
2022-01-18 19:30:37 -0500

    Add files via upload

---
+++ Week Two/exercise2_3.py
@@ -0,0 +1,4 @@
+hrs = input('Enter hours worked: ')
+rate = input('Enter pay rate: ')
+grossPay = float(hrs) * float(rate)
+print("Pay:", grossPay)
---
+++ Week Two/exercise2_5.py
@@ -0,0 +1,3 @@
+celsius = int(input('What is the temperature in celsius?'))
+fahrenheit = (celsius * 9/5) + 32
+print("This is your temperature in fahrenheit", farenheit)
